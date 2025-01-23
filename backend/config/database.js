ed16ff6863f27d8b66274330924e53818b07858c
PatrikMana <patrik.mana07@gmail.com>
2025-01-23 16:43:01 +0100

    1 den hotovo

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 // Připojení k databázi 'fintrack_db' s uživatelem 'root' a heslem 'root'.
-const sequelize = new Sequelize('undeath-db', 'root', 'root', {
+const sequelize = new Sequelize('undeath-db', 'root', 'Password123', {
   host: 'localhost',
   dialect: 'mysql',
 });
